cd2b0eb5bae21e12f49f701b0b62662f378970d6
jnrp20 <jnicolasromero@ucundinamarca.edu.co>
2024-05-12 21:28:19 -0500

    primercommit

---
+++ app/componentes/Main.tsx
@@ -0,0 +1,22 @@
+
+export default function Principal() {
+    return (
+      <div className="Container">
+        <nav>
+          <div className="NavLogo">
+              <img src='./trueno.png' alt="" />
+              <h1>CashierPro</h1>
+          </div>
+          <div className="NavOptions">
+            <a href="">Donaciones</a>
+            <a href="">Sobre CashierPro</a>
+            <a href="">Nuestras Redes</a>
+            <a href="">Contactanos</a>
+          </div>
+          <div className="NavRegister">
+              <a href="">Resgistrarse</a>
+          </div>
+        </nav>
+      </div>
+    );
+  }
